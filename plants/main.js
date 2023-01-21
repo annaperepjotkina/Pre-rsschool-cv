c3ee192e288b704f6958416bce55cce59db14d42
annaperepjotkina <5687538@gmail.com>
2023-01-21 20:58:21 +0300

    feat: create tablet version of plants

---
+++ plants/main.js
@@ -0,0 +1,10 @@
+    (function(){
+        const burgerItem = document.querySelector('.header__burger');
+        console.log(burgerItem);
+    }())
+
+    let a = jQuery(h1);
+console.log(a);
+
+    
+    
