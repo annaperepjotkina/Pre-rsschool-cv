a1eac9808dbea9e43dbf4c470c027ec8ea357533
annaperepjotkina <5687538@gmail.com>
2023-01-22 19:51:10 +0300

    feat: create  mobile version

--- plants/main.js
+++ plants/main.js
@@ -1,10 +1,17 @@
-    (function(){
-        const burgerItem = document.querySelector('.header__burger');
-        console.log(burgerItem);
+(function () {
+    const burgerItem = document.querySelector('.header__burger');
+    const menu = document.querySelector('.nav');
+    const menuCloseItem = document.querySelector('.header__nav-close');
+    const menuItem = document.querySelector('.nav-list');
+    burgerItem.addEventListener('click', () => {
+        menu.classList.add('nav_active');
+        });
+
+        menuItem.addEventListener('click', () => {
+            menu.classList.remove('nav_active');
+        });
+
+        menuCloseItem.addEventListener('click', () => {
+            menu.classList.remove('nav_active');
+        });
     }())
-
-    let a = jQuery(h1);
-console.log(a);
-
-    
-    
